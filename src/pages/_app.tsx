90d0a37a69b7bd416c248f20ec7db9ce26aa4694
LightBoy01 <mikailnururrahmon@gmail.com>
2025-07-24 11:56:34 +0100

    fix: remove unused Head import from _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
+
 import { Analytics } from '@vercel/analytics/next';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
